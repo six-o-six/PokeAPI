2081472cd664efc88be5290b62ce84bacc0d2d25
Jak <jakelynecavalcanti@gmail.com>
2026-05-15 20:45:02 -0300

    feat: Adicionar função para apagar downloads das imagens

---
+++ apagar_download.py
@@ -0,0 +1,12 @@
+import os
+
+def limpar_imagens():
+    pasta = 'img'
+    if os.path.exists(pasta):
+        for arquivo in os.listdir(pasta):
+            caminho_arquivo = os.path.join(pasta, arquivo)
+            try:
+                if os.path.isfile(caminho_arquivo):
+                    os.remove(caminho_arquivo)
+            except Exception as e:
+                print(f"Erro ao apagar {arquivo}: {e}")
