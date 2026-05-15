27e2803935536e95aa76304577b4173358a8e619
Jak <jakelynecavalcanti@gmail.com>
2026-05-15 20:46:38 -0300

    feat: Adicionar função de lógica sequencial

---
+++ sequencial.py
@@ -0,0 +1,23 @@
+import time
+from download_img import baixar_imagem_pokemon
+from apagar_download import limpar_imagens
+
+def sequencial(quantidade_downloads):
+    tempos = []
+    metodo = "Sequencial"
+    cpus = 1
+
+    for i in range(1, 11):
+        inicio = time.time()
+        for pokemon_id in range(1, quantidade_downloads + 1):
+            baixar_imagem_pokemon(pokemon_id)
+            
+        fim = time.time()
+        tempo_decorrido = fim - inicio
+        tempos.append(tempo_decorrido)
+        
+        print(f"{metodo} | {cpus} | {quantidade_downloads} | Tempo individual ({i}): {tempo_decorrido:.2f}s")
+        limpar_imagens()
+
+    tempo_medio = sum(tempos) / len(tempos)
+    print(f"{metodo} | {cpus} | {quantidade_downloads} | Tempo médio: {tempo_medio:.2f}s")
