9d0de81dc44d41567bafacb25214cffa876a629f
Jak <jakelynecavalcanti@gmail.com>
2026-05-16 02:30:55 -0300

    refactor: Integrar módulo de interface dinâmica no menu principal

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
-from sequencial import sequencial
-from multiprocessos import multiprocessos
-from threads import threads_executor
-from concorrencia import concorrencia
+import sequencial
+import multiprocessos
+import threads
+import concorrencia
+from interface_metodos import executar_interativo
 
 def main():
     print("-" * 30)
@@ -13,55 +14,19 @@ def main():
     print("3. Executar Threads")
     print("4. Executar Concorrência")
     print("5. Sair")
-
+    
     opcao = input("\nEscolha o método: ")
-
+    
     if opcao == '1':
-        try:
-            downloads = int(input("Quantidade de downloads (100, 500 ou 1000): "))
-            if downloads in [100, 500, 1000]:
-                sequencial(downloads)
-            else:
-                    print("Valor inválido para Downloads.")
-        except ValueError:
-            print("Insira um número válido.")
-    
+        executar_interativo(sequencial.sequencial)
     elif opcao == '2':
-        try:
-            cpus = int(input("Quantidade de CPUs (2, 4 ou 8): "))
-            downloads = int(input("Quantidade de downloads (100, 500 ou 1000): "))
-            if cpus in [2, 4, 8] and downloads in [100, 500, 1000]:
-                multiprocessos(cpus, downloads)
-            else:
-                print("Valores inválidos para CPU ou Downloads.")
-        except ValueError:
-            print("Insira números válidos.")
-            
+        executar_interativo(multiprocessos.multiprocessos)
     elif opcao == '3':
-        try:
-            cpus = int(input("Quantidade de CPUs (2, 4 ou 8): "))
-            downloads = int(input("Quantidade de downloads (100, 500 ou 1000): "))
-            if cpus in [2, 4, 8] and downloads in [100, 500, 1000]:
-                threads_executor(cpus, downloads)
-            else:
-                print("Valores inválidos para CPU ou Downloads.")
-        except ValueError:
-            print("Insira números válidos.")
-            
+        executar_interativo(threads.threads_executor)
     elif opcao == '4':
-        try:
-            cpus = int(input("Quantidade de CPUs (2, 4 ou 8): "))
-            downloads = int(input("Quantidade de downloads (100, 500 ou 1000): "))
-            if cpus in [2, 4, 8] and downloads in [100, 500, 1000]:
-                concorrencia(cpus, downloads)
-            else:
-                print("Valores inválidos para CPU ou Downloads.")
-        except ValueError:
-            print("Insira números válidos.")
-    
+        executar_interativo(concorrencia.concorrencia)
     elif opcao == '5':
         sys.exit()
-    
     else:
         print("Opção inválida.")
 
