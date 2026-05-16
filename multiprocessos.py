b2ffdf9a1abcdd055ac76dd76a5346a948a7e615
Jak <jakelynecavalcanti@gmail.com>
2026-05-16 02:25:41 -0300

    refactor: Integrar módulo de interface dinâmica no script multiprocessos

--- multiprocessos.py
+++ multiprocessos.py
@@ -2,6 +2,7 @@ import time
 from multiprocessing import Pool
 from download_img import baixar_imagem_pokemon
 from apagar_download import limpar_imagens
+from interface_metodos import executar_interativo
 
 def multiprocessos(n_cpus, quantidade_downloads):
     tempos = []
@@ -25,12 +26,4 @@ def multiprocessos(n_cpus, quantidade_downloads):
     print(f"{metodo} | {n_cpus} | {quantidade_downloads} | Tempo médio: {tempo_medio:.2f}s")
     
 if __name__ == "__main__":
-    try:
-        cpus = int(input("Quantidade de CPUs (2, 4 ou 8): "))
-        downloads = int(input("Quantidade de downloads (100, 500 ou 1000): "))
-        if cpus in [2, 4, 8] and downloads in [100, 500, 1000]:
-            multiprocessos(cpus, downloads)
-        else:
-            print("Valores inválidos para CPU ou Downloads.")
-    except ValueError:
-        print("Insira números válidos.")
+    executar_interativo(multiprocessos)
